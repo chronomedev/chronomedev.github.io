d8d473bd1afe2bbee8ba4e9d18db20c71d5503b2
Hansrenee Prayitno <hansrenee@live.com>
2020-09-30 00:26:40 +0700

    first upload

---
+++ js/load.js
@@ -0,0 +1,37 @@
+// load script to portfolio container
+
+window.onload = ()=>{
+    servicesTest();
+}
+function servicesTest(){
+
+    document.getElementById("status_proses").innerHTML = "Starting Chronome Portfolio container...";
+    $.ajax({  
+        type: 'POST',  
+        url: 'http://localhost:1998/on',
+        crossDomain: true,
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        dataType: "json",
+        data: "",
+        success: function(data_respon) {
+            if(data_respon["status"] == 1){
+                document.getElementById("status_proses").innerHTML = "Success!";
+                window.location.assign("https://chronomedev.herokuapp.com/");
+            } else {
+                $("#game_service_loader").hide();
+                document.getElementById("status_proses").innerHTML = "Failed!";
+                $(".unslate_co--overlayer").hide();
+                document.getElementById("img_loading").src = "images/chibi_angry_by_sakuretta94.png";
+                window.alert("Servis sedang tidak merespon harap coba kembali nanti");
+            }
+        },
+        error : function(){
+            window.alert("gagal");
+            $("#game_service_loader").hide();
+            document.getElementById("img_loading").src = "images/chibi_angry_by_sakuretta94_d7kj0os-fullview.png"
+            document.getElementById("status_proses").innerHTML = "Failed!";
+        }
+    });
+}
